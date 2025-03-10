351250e645324daccd3ec307b5190e9d83440c53
Thomas Rieu <rieu.thomas@hotmail.com>
2025-03-10 12:10:04 +0100

    first-commit

---
+++ my-app/src/Checkboard.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import './Checkboard.css';
+
+
+export default function CheckBoard() {
+
+    return (
+        <div className="container"> Hello
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+            <div className="white"></div>
+            <div className="black"></div>
+
+        </div>
+
+
+    )
+}
